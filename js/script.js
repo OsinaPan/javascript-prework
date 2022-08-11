5d3c77b1437e1e4e58447419e23b5300dcd5faa3
Michal Osinski <michal.osinski94@gmail.com>
2022-08-11 19:58:57 +0200

    add change js v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+let computerMove = `papier`;
+
+printMessage('Zagrałem' + computerMove + 'Jeśli Twój ruch to kamień, to wygrywasz!');
+
+let computerMove = `nożyce`;
