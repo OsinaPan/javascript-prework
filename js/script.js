377899d0a228b3e429f3e2869eeda1a75f8c717f
Michal Osinski <michal.osinski94@gmail.com>
2022-08-11 20:43:28 +0200

    delete all content in scripts.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let computerMove = `kamień`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
-
-let playerMove = `papier`;
-
-printMessage('Zagrałem ' + playerMove + '! Wygrywam!');
