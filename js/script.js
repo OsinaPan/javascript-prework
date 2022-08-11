f61cfc946bb7cb8351630b4d26cb03e8711c9c96
Michal Osinski <michal.osinski94@gmail.com>
2022-08-11 20:14:06 +0200

    add new printMessage

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ let computerMove = `kamień`;
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
 
 let playerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove + '! Wygrywam!');
