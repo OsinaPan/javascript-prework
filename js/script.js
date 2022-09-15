6ba1182523d9e9f3828a131e8f4462f59375a103
Michal Osinski <michal.osinski94@gmail.com>
2022-09-15 15:44:44 +0200

    addListeners

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-function clearMessages(){
-    
+function clearMessages(){   
 }
 
 function playGame(playerInput){;
@@ -40,9 +39,16 @@ let computerMove = getMoveName(randomNumber);
     printMessage('Mój ruch to: ' + computerMove);
 
 /*gracz*/
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+//let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 let playerMove = getMoveName(playerInput);
     printMessage('Twój ruch to: ' + playerMove);
 
 // wynik gry //
 displayResult(computerMove, playerMove);
+
+document.getElementById('play-rock').addEventListener('click', function(){playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function(){playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function(){playGame(3);
+});
